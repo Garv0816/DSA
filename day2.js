d89c45fb11d5174fd2dfd0f7ffb34dd035d8b735
Garv0816 <garv020596@gmail.com>
2025-10-01 19:36:44 +0530

    yoo

--- day2.js
+++ day2.js
@@ -1,32 +1,39 @@
-const arr = [4,5,2,555,1221 ,7,9,2]
-let num = -Infinity
-let secNum = -Infinity
-for (let i=0 ; i<arr.length; i++){
+// const arr = [4,5,2,555,1221 ,7,9,2]
+// let num = -Infinity
+// let secNum = -Infinity
+// for (let i=0 ; i<arr.length; i++){
     
-    if(num < arr[i]){
-        secNum = num;
-        num = arr[i];
+//     if(num < arr[i]){
+//         secNum = num;
+//         num = arr[i];
         
 
        
         
 
-    }
-
-
-}
-console.log(num)
-console.log(secNum)
-
-
-
-// const arr2 = [7,9,2]
-
-// let secLargest= 0;
-// for (let i=0; i<arr2.length;i++){
-//     if(secLargest < arr2.length)
-//     {
-//         secLargest= arr2[i]
 //     }
+
+
 // }
-// console.log(secLargest)
+// console.log(num)
+// console.log(secNum)
+
+
+// let num = -768;
+// // let arr = String(num).split('')
+// // console.log(arr.length)
+// let count = 0
+// while(num>1){
+
+//     num = num/10
+//     count++
+    
+    
+   
+// }
+
+// console.log(count)
+
+
+
+
